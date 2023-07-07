4a56fbc174aeb690904c191628d6d9ec03ad6b36
mikyleravenn <120079429+mikyleravenn@users.noreply.github.com>
2023-07-07 15:33:01 +0800

    Add files via upload

---
+++ FER/main.py
@@ -0,0 +1,23 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+import cv2
+from deepface import DeepFace
+
+face_classifier = cv2.CascadeClassifier()
+face_classifier.load(cv2.samples.findFile("haarcascade_frontalface_default.xml"))
+
+cap = cv2.VideoCapture(0)
+while True:
+    ret, frame = cap.read()
+    frame_gray =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(frame_gray)
+    response = DeepFace.analyze(frame, actions=("emotion",), enforce_detection=False)
+    for face in faces:
+        x, y, w, h = face
+        cv2.putText(frame, text = response["dominant_emotion"], org=(x,y), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(255, 0, 0))
+        new_frame = cv2.rectangle(frame_gray, (x, y), (x+w,y+h), color=(255,0,0), thickness=2)
+    cv2.imshow("", new_frame)
+    if (cv2.waitKey(30) == 27):
+        break
+    cap.release()
+    cv2.destroyAllWindows()
