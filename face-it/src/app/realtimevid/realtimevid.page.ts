574c16f0d67f47269a6afc83bff443959d285855
John Joseph Mangampat <93865043+jaymangampat@users.noreply.github.com>
2023-07-10 14:00:11 +0800

    Update realtimevid.page.ts

--- face-it/src/app/realtimevid/realtimevid.page.ts
+++ face-it/src/app/realtimevid/realtimevid.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-realtimevid',
   templateUrl: './realtimevid.page.html',
   styleUrls: ['./realtimevid.page.scss'],
 })
-export class RealtimevidPage implements OnInit {
+export class RealtimevidPage {
+  isFullscreen = false;
+  icon: string = 'expand';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
+  toggleFullscreen() {
+    if (document.fullscreenElement) {
+      document
+        .exitFullscreen()
+        .then(() => console.log("Document exited from full-screen mode"))
+        .catch((err) => console.error(err));
+    } else {
+      document.documentElement.requestFullscreen();
+    }
   }
 
+  @HostListener('fullscreenchange', ['$event'])
+  handleFullscreenChange(event: Event) {
+    this.isFullscreen = !!document.fullscreenElement;
+    this.updateIcon();
+  }
+
+  updateIcon() {
+    this.icon = this.isFullscreen ? 'contract' : 'expand';
+  }
 }
