11c96cd0a2f6e62b9db52d68a0f1766fd9e3704d
John Joseph Mangampat <93865043+jaymangampat@users.noreply.github.com>
2023-07-10 15:46:31 +0800

    Update realtimevid.page.ts

--- face-it/src/app/realtimevid/realtimevid.page.ts
+++ face-it/src/app/realtimevid/realtimevid.page.ts
@@ -1,4 +1,5 @@
 import { Component, HostListener } from '@angular/core';
+import { AnimationController, ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-realtimevid',
@@ -8,8 +9,51 @@ import { Component, HostListener } from '@angular/core';
 export class RealtimevidPage {
   isFullscreen = false;
   icon: string = 'expand';
+  videoDevices: MediaDeviceInfo[] = []; // Array to store the available video devices
+  selectedVideoDevice: string = ''; // Variable to store the selected video device
 
-  constructor() {}
+  constructor(
+    private animationCtrl: AnimationController,
+    private modalController: ModalController
+  ) {}
+
+  getVideoDevices() {
+    navigator.mediaDevices.enumerateDevices().then(devices => {
+      this.videoDevices = devices.filter(device => device.kind === 'videoinput');
+    });
+  }
+
+  // Call the getVideoDevices() method when the component initializes
+  ngOnInit() {
+    this.getVideoDevices();
+  }
+  enterAnimation = (baseEl: HTMLElement) => {
+    const root = baseEl.shadowRoot;
+
+    const backdropAnimation = this.animationCtrl
+      .create()
+      .addElement(root?.querySelector('ion-backdrop')!)
+      .fromTo('opacity', '0.01', 'var(--backdrop-opacity)');
+
+    const wrapperAnimation = this.animationCtrl
+      .create()
+      .addElement(root?.querySelector('.modal-wrapper')!)
+      .keyframes([
+        { offset: 0, opacity: '0', transform: 'scale(0)' },
+        { offset: 1, opacity: '0.99', transform: 'scale(1)' },
+      ]);
+
+    return this.animationCtrl
+      .create()
+      .addElement(baseEl)
+      .easing('ease-out')
+      .duration(250)
+      .addAnimation([backdropAnimation, wrapperAnimation]);
+  };
+
+  leaveAnimation = (baseEl: HTMLElement) => {
+    return this.enterAnimation(baseEl).direction('reverse');
+  };
 
   toggleFullscreen() {
     if (document.fullscreenElement) {
@@ -30,5 +74,8 @@ export class RealtimevidPage {
 
   updateIcon() {
     this.icon = this.isFullscreen ? 'contract' : 'expand';
+  }
+  closeModal() {
+    this.modalController.dismiss();
   }
 }
