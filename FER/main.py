4cbc97f311473d4a2ac50b736ec0b8e12fd033cd
mikyleravenn <120079429+mikyleravenn@users.noreply.github.com>
2023-07-12 10:11:31 +0800

    Update main.py

--- FER/main.py
+++ FER/main.py
@@ -2,6 +2,7 @@ import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 import cv2
 import matplotlib.pyplot as plt
+import time
 from deepface import DeepFace
 
 face_classifier = cv2.CascadeClassifier()
@@ -10,6 +11,10 @@ face_classifier.load(cv2.samples.findFile("haarcascade_frontalface_default.xml")
 cap = cv2.VideoCapture(0)
 emotions_count = {"Happy": 0, "Neutral": 0, "Angry": 0}  # Initialize emotions count dictionary
 desired_emotions = ["happy", "neutral", "angry"]  # Desired emotions to detect
+
+start_time = time.time()
+interval = 3  # 3 seconds interval for emotion count
+
 while True:
     try:
         ret, frame = cap.read()
@@ -26,13 +31,20 @@ while True:
             if dominant_emotion in desired_emotions:  # Check if the emotion is in the desired emotions
                 emotions_count[dominant_emotion.capitalize()] += 1  # Increment the count for the detected emotion
 
-                cv2.putText(frame, text=dominant_emotion.capitalize(), org=(x, y), fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(0, 255, 0))
-                cv2.rectangle(frame, (x, y), (x+w, y+h), color=(255, 0, 0), thickness=2)
+                cv2.putText(frame, text=dominant_emotion.capitalize(), org=(x, y),
+                            fontFace=cv2.FONT_HERSHEY_COMPLEX, fontScale=1, color=(0, 255, 0))
+                cv2.rectangle(frame, (x, y), (x + w, y + h), color=(255, 0, 0), thickness=2)
 
         cv2.imshow("", frame)
 
         if cv2.waitKey(30) == 27:
             break
+
+        elapsed_time = time.time() - start_time
+        if elapsed_time >= interval:
+            print("Emotion Count:", emotions_count)
+            emotions_count = {"Happy": 0, "Neutral": 0, "Angry": 0}  # Reset emotions count
+            start_time = time.time()
 
     except KeyboardInterrupt:
         break
@@ -40,12 +52,10 @@ while True:
 cap.release()
 cv2.destroyAllWindows()
 
-# Create a bar chart for emotions count
+# Create a pie chart for emotions count
 emotions = list(emotions_count.keys())
 count = list(emotions_count.values())
 
-plt.bar(emotions, count)
-plt.xlabel('Emotions')
-plt.ylabel('Count')
-plt.title('Detected Emotions')
+plt.pie(count, labels=emotions, autopct='%1.1f%%')
+plt.title('Feedback Chart')
 plt.show()
