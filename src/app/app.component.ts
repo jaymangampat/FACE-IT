369e173c0d01044699a9def466c87627ba8bbd8e
John Joseph Mangampat <93865043+jaymangampat@users.noreply.github.com>
2023-07-13 21:28:07 +0800

    add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -9,5 +12,11 @@ export class AppComponent {
     { title: 'Dashboard', url: '/dashboard', icon: 'analytics' },
     { title: 'Real-Time Video', url: '/realtimevid', icon: 'videocam' },
   ];
-  constructor() {}
+
+  constructor(private router: Router, private menuController: MenuController) {}
+
+  logout() {
+    this.menuController.close();
+    this.router.navigateByUrl('/login', { replaceUrl: true });
+  }
 }
