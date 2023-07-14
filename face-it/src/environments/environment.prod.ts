225ab5430c922be2e94af711b0113b67f37ebee9
John Joseph Mangampat <93865043+jaymangampat@users.noreply.github.com>
2023-07-14 16:32:27 +0800

    Update environment.prod.ts

--- face-it/src/environments/environment.prod.ts
+++ face-it/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCVV451DBMF03ILkbCpfCeLUdu5wBlkMf8",
+    authDomain: "capstone-74e48.firebaseapp.com",
+    databaseURL: "https://capstone-74e48-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "capstone-74e48",
+    storageBucket: "capstone-74e48.appspot.com",
+    messagingSenderId: "491354500869",
+    appId: "1:491354500869:web:da7709a12f914096e4116c",
+    measurementId: "G-2T7FQK4R4T"
+  }
 };
